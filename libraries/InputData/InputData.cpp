91595d69967a5d2a65d68a2c3cf62587149c3c20
studentmorrisjohn <74033565+studentmorrisjohn@users.noreply.github.com>
2022-07-13 08:49:25 +0800

    Add files via upload

--- libraries/InputData/InputData.cpp
+++ libraries/InputData/InputData.cpp
@@ -4,13 +4,22 @@
 #include <FarSensors.h>
 #include <Directions.h>
 #include <Triggers.h>
+#include <Adafruit_TCS34725.h>
 
-InputData::InputData(LineFollower lineFollower, FarSensors farSensor) : 
-	_lineFollower(lineFollower), _farSensors(farSensor) {}
+InputData::InputData(LineFollower lineFollower, FarSensors farSensor, Adafruit_TCS34725 tcs) : 
+	_lineFollower(lineFollower), _farSensors(farSensor), _colorSensor(tcs) {}
 
 void InputData::initialize() {
 	_lineFollower.initialize();
 	_farSensors.initialize();
+
+	if (_colorSensor.begin()) {
+    Serial.println("Found sensor");
+	} else {
+		Serial.println("No TCS34725 found ... check your connections");
+		while (1);
+	}
+
 }
 
 int InputData::getTrigger() {
@@ -27,9 +36,79 @@ int InputData::getError() {
 	return _error;
 }
 
+bool InputData::noLine() {
+	return _lineFollower.noLine();
+}
+
+bool InputData::hasIntersection() {
+	return _lineFollower.hasIntersection();
+}
+
+bool InputData::withinThreshold(float value, float min, float max) {
+	return (value > min) && (value < max);
+}
+
+
+int InputData::readColor() {
+	uint16_t red, green, blue, clear;
+	
+	_colorSensor.getRawData(&red, &green, &blue, &clear);
+
+	uint32_t sum = red;
+
+	sum += blue + green;
+
+	float r, g, b;
+
+	r = red; r /= sum;
+	g = green; g /= sum;
+	b = blue; b /= sum;
+	r *= 256; g *= 256; b *= 256;
+
+	if (r > 255) r = 255;
+	if (g > 255) g = 255;
+	if (b > 255) b = 255;
+
+	float remove, normalize;
+	if ((b < g) && (b < r)) {
+		remove = b;
+		normalize = max(r-b, g-b);
+	} else if ((g < b) && (g < r)) {
+		remove = g;
+		normalize = max(r-g, b-g);
+	} else {
+		remove = r;
+		normalize = max(b-r, g-r);
+	}
+	// get rid of minority report
+	float rednorm = r - remove;
+	float greennorm = g - remove;
+	float bluenorm = b - remove;
+	// now normalize for the highest number
+	rednorm /= normalize;
+	greennorm /= normalize;
+	bluenorm /= normalize;
+
+	if (rednorm >= 1) {
+		return 1;
+	}
+
+	return 0;
+}
+
+bool InputData::hasRed() {
+	return readColor();
+}
+
 void InputData::readSensors() {
 	_lineFollower.readSensors();
 	_farSensors.readSensors();
+
+	if (hasRed()) {
+		_trigger = RED_DETECTED;
+		_error = 0;
+		return;
+	}
 
 	if (_farSensors.hasLeft() || _farSensors.hasRight()) {
 		_trigger = TURN;
@@ -56,15 +135,6 @@ void InputData::readSensors() {
 		_error = 0;
 		return;
 	}
-
-	// Implement this after adding color sensor
-	//if (_colorSensor.hasRed()) {
-	//	return;
-	//}
-
-	//if (_colorSensor.hasBlue()) {
-	//	return;
-	//}
 
 	_trigger = FOLLOW_LINE;
 	_error = _lineFollower.getError();
--- libraries/InputData/InputData.h
+++ libraries/InputData/InputData.h
@@ -9,12 +9,21 @@
 
 #include <LineFollower.h>
 #include <FarSensors.h>
+#include <Adafruit_TCS34725.h>
 
+#define BLUE_MAX (int[]){350, 400, 400}
+#define BLUE_MIN (int[]){150, 200, 200}
+#define RED_MAX (int[]){600, 500, 500}
+#define RED_MIN (int[]){350, 300, 300}
+
+#define RED 0
+#define BLUE 1
+#define OTHERS 2
 
 class InputData {
 public:
     // Constructor
-    InputData(LineFollower lineFollower, FarSensors farSensor);
+    InputData(LineFollower lineFollower, FarSensors farSensor, Adafruit_TCS34725 tcs);
 
     // Methods
     void initialize();
@@ -22,11 +31,17 @@ public:
     int getTrigger();
     int getDirection();
     int getError();
+    int readColor();
+    bool withinThreshold(float value, float min, float max);
+    bool hasRed();
+    bool noLine();
+    bool hasIntersection();
 
 private:
     LineFollower _lineFollower;
     FarSensors _farSensors;
     int _trigger, _direction, _error;
+    Adafruit_TCS34725 _colorSensor;
 };
 
 #endif
